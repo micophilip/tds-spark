6f5e1add212b3f82d1a5c63fb0b83f57f783763a
mico <micophilip@gmail.com>
2019-10-16 22:49:38 -0400

    read in adult dataset

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Top-Down Specialization on Apache Spark&trade;
 
 Proposed top-down specialization algorithm on Apache Spark for COMP 5704
+
+# Build
+
+Run `build.sbt`
+
+# Package
+
+Run `sbt package` task
+
+# Run
+
+## Spark submit
+
+`spark-submit --class TopDownSpecialization --master local target/scala-2.12/code_2.12-0.1.jar <pathToInputDataset>`
--- src/main/scala/TopDownSpecialization.scala
+++ src/main/scala/TopDownSpecialization.scala
@@ -2,7 +2,22 @@ import org.apache.spark.SparkContext
 import org.apache.spark.sql.SparkSession
 
 object TopDownSpecialization extends Serializable {
+
   def main(args: Array[String]) = {
-    val spark = SparkSession.builder().appName("TopDownSpecialization").getOrCreate()
+
+    val inputPath = args(0)
+
+    val spark = SparkSession.builder().appName("TopDownSpecialization")
+      .config("spark.master", "local").getOrCreate()
+    val input = spark.read.format("csv")
+      .option("header", "true")
+      .option("mode", "FAILFAST")
+      .option("inferSchema", "true")
+      .load(inputPath)
+
+    input.show()
+
+    spark.stop()
   }
+
 }
