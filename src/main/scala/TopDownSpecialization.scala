e927e0f1de913e2f68090fbcae8aee46751d0f55
mico <micophilip@gmail.com>
2019-10-17 22:43:16 -0400

    preprocessing step

--- src/main/scala/TopDownSpecialization.scala
+++ src/main/scala/TopDownSpecialization.scala
@@ -1,11 +1,14 @@
 import org.apache.spark.SparkContext
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.column
+import org.apache.spark.sql.{Column, DataFrame, SparkSession}
 
 object TopDownSpecialization extends Serializable {
 
   def main(args: Array[String]) = {
 
     val inputPath = args(0)
+
+    val QIDs = List("age", "education", "marital-status", "occupation", "native-country")
 
     val spark = SparkSession.builder().appName("TopDownSpecialization")
       .config("spark.master", "local").getOrCreate()
@@ -15,7 +18,11 @@ object TopDownSpecialization extends Serializable {
       .option("inferSchema", "true")
       .load(inputPath)
 
-    input.show()
+    val subset = input.select(QIDs.head, QIDs.tail: _*)
+
+    val subsetWithK = subset.groupBy(QIDs.head, QIDs.tail: _*).count()
+
+    subsetWithK.show()
 
     spark.stop()
   }
