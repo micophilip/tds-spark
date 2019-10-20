da41c547ddb04eafaa602119e61c89dab02dfc58
mico <micophilip@gmail.com>
2019-10-20 17:24:23 -0400

    generate taxonomy trees

--- build.sbt
+++ build.sbt
@@ -3,8 +3,10 @@ version := "0.1"
 scalaVersion := "2.12.10"
 
 val sparkVersion = "2.4.4"
+val argonautVersion = "6.2.2"
 
 libraryDependencies ++= Seq (
   "org.apache.spark" %% "spark-core" % sparkVersion,
-  "org.apache.spark" %% "spark-sql" % sparkVersion
+  "org.apache.spark" %% "spark-sql" % sparkVersion,
+  "io.argonaut" %% "argonaut" % argonautVersion
 )
--- src/main/scala/TopDownSpecialization.scala
+++ src/main/scala/TopDownSpecialization.scala
@@ -1,28 +1,52 @@
-import org.apache.spark.SparkContext
-import org.apache.spark.sql.functions.column
-import org.apache.spark.sql.{Column, DataFrame, SparkSession}
+import argonaut.Argonaut._
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+import scala.io.Source
 
 object TopDownSpecialization extends Serializable {
 
   def main(args: Array[String]) = {
 
+    val spark = SparkSession.builder().appName("TopDownSpecialization")
+      .config("spark.master", "local").getOrCreate()
+
     val inputPath = args(0)
+    val taxonomyTreePath = args(1)
+    val k = args(2)
+    val sensitiveAttributeColumn = args(3)
+
+    println(s"Anonymizing dataset in $inputPath")
+    println(s"Running TDS with k = $k")
 
     val QIDs = List("age", "education", "marital-status", "occupation", "native-country")
 
-    val spark = SparkSession.builder().appName("TopDownSpecialization")
-      .config("spark.master", "local").getOrCreate()
-    val input = spark.read.format("csv")
+    val input = spark.read
       .option("header", "true")
       .option("mode", "FAILFAST")
       .option("inferSchema", "true")
-      .load(inputPath)
+      .csv(inputPath)
 
-    val subset = input.select(QIDs.head, QIDs.tail: _*)
+    /*
+     * Step 1: Pre-processing
+     */
 
+    // Step 1.0: Store sensitive attributes
+    val sensitiveAttributes: List[String] = input.select(sensitiveAttributeColumn).distinct().collect().map(_.getString(0)).toList
+
+    // Step 1.1: All non quasi-identifier attributes are removed
+    val subset = input.select(QIDs.head, QIDs.tail: _*) //Pass each element of QID as its own argument to select
+
+    // Step 1.2: Tuples with the same quasi-identifier values are grouped together
     val subsetWithK = subset.groupBy(QIDs.head, QIDs.tail: _*).count()
 
-    subsetWithK.show()
+    /*
+     * Step 2: Generalization
+     */
+
+    val taxonomyTreeSource = Source.fromFile(taxonomyTreePath)
+    val taxonomyTreeString = try taxonomyTreeSource.mkString finally taxonomyTreeSource.close()
+    val anonymizationLevels = taxonomyTreeString.parseOption.get
 
     spark.stop()
   }
